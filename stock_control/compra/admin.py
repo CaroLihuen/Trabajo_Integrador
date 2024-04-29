8a6b54da389d7c6c1e7cc2a51d61f8ddf4f21d99
CaroLihuen <carolinaldominguez@outlook.com>
2024-04-29 16:30:08 -0300

    change admin

--- stock_control/compra/admin.py
+++ stock_control/compra/admin.py
@@ -4,9 +4,13 @@ from compra.models import Producto,Proveedor
 # Register your models here.
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'precio', 'stock_actual']
+    search_fields = ['nombre', 'precio', 'stock_actual']
     pass
 
 @admin.register(Proveedor)
 class ProveedorAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'apellido', 'dni']
+    search_fields = ['nombre', 'apellido', 'dni']
     pass
     
