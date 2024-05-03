40c607f743bc3e64281564fb721ca9180252d9ef
Carolina <carolinaldominguez@outlook.com>
2024-05-03 15:06:57 -0300

    adding templates static

--- stock_control/compra/urls.py
+++ stock_control/compra/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
-app_name = 'compra' 
+app_name = 'compra'  
 
 #urls del entorno de compra
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='base/compra.html'), name='compra'),
     path('lista_productos/', views.lista_productos, name='lista_productos'),
     path('detalle_producto/<int:pk>/', views.detalle_producto, name='detalle_producto'),
     path('crear_producto/', views.crear_producto, name='crear_producto'),
--- stock_control/stock_control/urls.py
+++ stock_control/stock_control/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('compra/', include('compra.urls', namespace='compra'))
+    path('compra/', include('compra.urls', namespace='compra')),
+    path('', TemplateView.as_view(template_name='base/home.html'), name='home')
 ]
